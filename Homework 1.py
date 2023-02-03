a071a6a60d2169313ef338809f5c4e2928243e38
Sarah Stephenson <123026466+sstephenson01@users.noreply.github.com>
2023-02-03 10:47:50 -0500

    Add files via upload

---
+++ Homework 1.py
@@ -0,0 +1,24 @@
+#Sarah Stephenson
+#Problem 1
+type("Rosewater")
+type(0.001)
+type(37)
+type(False)
+#Problem 2
+print("my","name","is","Sarah")
+print("my"+"name"+"is"+"Sarah")
+#Problem 3
+str(14)
+int(3.5)
+float(4)
+bool(1)
+#Problem 4
+print("hi\nmy name is Sarah\nI am a senior\nMy favorite color is blue\ngoodbye")
+#Problem 5
+flour=2
+milk=1
+egg=4
+oil=.4
+vanilla=.012
+cake=2*flour+0.5*(milk-egg+vanilla)+oil**2
+print(cake)
